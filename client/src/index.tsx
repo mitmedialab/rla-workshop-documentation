7ec91526d4555a4ae05e66825d686a1849b30bf3
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2018-01-20 16:21:07 +0200

    update schedule

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -14,14 +14,10 @@ class App extends React.Component<any, any> {
         <ul className="uk-subnav uk-subnav-pill" uk-switcher="connect: .switcher-container">
           <li><a href="#"><span uk-icon="icon: calendar;"></span> Schedule</a></li>
           <li><a href="#"><span uk-icon="icon: users;"></span> Office Hours</a></li>
-          <li><a href="#"><span uk-icon="icon: folder;"></span> Documents</a></li>
-          <li><a href="#"><span uk-icon="icon: file-edit;"></span> Journal</a></li>
         </ul>
         <ul className="uk-margin uk-switcher switcher-container">
           <li><Schedule /></li>
           <li><OfficeHours /></li>
-          <li><Documents /></li>
-          <li><Journal /></li>
         </ul>
       </div>
     );
