78ad9d92d2e0e12da8763c460e5c7a36a74e4117
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2018-01-22 14:19:36 +0200

    reorganize documents

--- client/src/routes/Documents.tsx
+++ client/src/routes/Documents.tsx
@@ -11,16 +11,12 @@ class Documents extends React.Component<any, any> {
         <table className="uk-table uk-table-small uk-table-middle">
           <tbody>
             <tr>
-              <td className="limit-width"><a className="uk-button uk-button-primary" href="https://drive.google.com/drive/folders/14d8G6Q1aWkhHrHbOMju0Hh8XglqZLVjz?usp=sharing">Field Day</a></td>
-              <td>Documents from Sunday's field visits</td>
+              <td className="limit-width"><a className="uk-button uk-button-primary" href="https://drive.google.com/open?id=1CFoO69vGhimnAozX2Gdl4WaIDNpVpH4H">Sessions</a></td>
+              <td>Slide decks and worksheets from throughout the week</td>
             </tr>
             <tr>
-              <td className="limit-width"><a className="uk-button uk-button-primary" href="https://drive.google.com/drive/folders/1bMuOHSuXzpQQ0ZqPuP07GCTaGuRlmMSq?usp=sharing">Interview Notes</a></td>
-              <td>Learnings from interviews in the field</td>
-            </tr>
-            <tr>
-              <td className="limit-width"><a className="uk-button uk-button-primary" href="https://drive.google.com/open?id=1CFoO69vGhimnAozX2Gdl4WaIDNpVpH4H">Slides</a></td>
-              <td>Slide decks from presentations throughout the week</td>
+              <td className="limit-width"><a className="uk-button uk-button-primary" href="https://drive.google.com/drive/folders/1bMuOHSuXzpQQ0ZqPuP07GCTaGuRlmMSq?usp=sharing">Learnings</a></td>
+              <td>General notes from from field interviews and NGO discussions</td>
             </tr>
           </tbody>
         </table>
