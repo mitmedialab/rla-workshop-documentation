85292911f674783647abc65a4e5a6cad14f9d365
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2018-01-23 09:50:18 +0200

    add teams document

--- client/src/routes/Documents.tsx
+++ client/src/routes/Documents.tsx
@@ -11,11 +11,15 @@ class Documents extends React.Component<any, any> {
         <table className="uk-table uk-table-small uk-table-middle">
           <tbody>
             <tr>
-              <td className="limit-width"><a className="uk-button uk-button-primary" href="https://drive.google.com/open?id=1CFoO69vGhimnAozX2Gdl4WaIDNpVpH4H">Sessions</a></td>
+              <td className="limit-width"><a className="uk-button uk-button-primary document-button" href="https://drive.google.com/open?id=1CFoO69vGhimnAozX2Gdl4WaIDNpVpH4H">Teams</a></td>
+              <td>Names, members, and descriptions of all workshop teams</td>
+            </tr>
+            <tr>
+              <td className="limit-width"><a className="uk-button uk-button-primary document-button" href="https://drive.google.com/open?id=1CFoO69vGhimnAozX2Gdl4WaIDNpVpH4H">Sessions</a></td>
               <td>Slide decks and worksheets from throughout the week</td>
             </tr>
             <tr>
-              <td className="limit-width"><a className="uk-button uk-button-primary" href="https://drive.google.com/drive/folders/1bMuOHSuXzpQQ0ZqPuP07GCTaGuRlmMSq?usp=sharing">Learnings</a></td>
+              <td className="limit-width"><a className="uk-button uk-button-primary document-button" href="https://drive.google.com/drive/folders/1bMuOHSuXzpQQ0ZqPuP07GCTaGuRlmMSq?usp=sharing">Learnings</a></td>
               <td>General notes from from field interviews and NGO discussions</td>
             </tr>
           </tbody>
